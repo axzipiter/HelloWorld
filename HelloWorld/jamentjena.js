7f013d7c23971e14efdb885dc2d533726800841b
Erik Pettersson <axzipiter@hotmail.com>
2017-03-28 11:21:44 +0200

    Add files via upload

---
+++ HelloWorld/jamentjena.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Axzipiter on 2017-03-28.
+ */
+var hej = "Hello World! ligger detta i branchen?";
+
+function hejsan() {
+    while(true) {
+        console.log(hej);
+    }
+}
+
+hejsan();
